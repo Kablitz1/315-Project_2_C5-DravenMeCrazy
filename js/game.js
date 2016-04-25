e5318e4df20a09cf53b1b7dca9d3ffe9a4d03908
dylanbarnes65 <dylan.barnes65@yahoo.com>
2016-04-25 11:27:52 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -558,7 +558,7 @@ placeTurret = function(){
 			game.load.image('settingsbutton',asset_location + 'settingsbutton.jpg');
             
             //menus
-            game.load.image('upgrade_menu2', asset_location + 'upgrade_menu2.png');
+            game.load.image('upgrade_menu', asset_location + 'upgrade_menu.png');
             game.load.image('option_menu', asset_location + 'options_menu.png');
             game.load.image('help_menu', asset_location + 'help_menu.png');
 
